eb7ccfb7f5e971eec9eb8f54014038f5628d954d
DavidQL <david@quickleft.com>
2011-01-06 22:29:42 -0800

    merged variables into one init block

--- jquery.pushTop.js
+++ jquery.pushTop.js
@@ -1,8 +1,8 @@
 (function( $ ){
   $.fn.pushTop = function() {
     return this.each(function() {
-      var $this = $(this);
-      top_z_index = 0;
+      var $this = $(this),
+          top_z_index = 0;
       $('*').each(function() {
         var this_z_index = $(this).css('z-index');
         if (this_z_index != "auto" && this_z_index > top_z_index) {
